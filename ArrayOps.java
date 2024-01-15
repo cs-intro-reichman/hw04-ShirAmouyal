2c8a24130053e148397dacb0360b810c44c1b5a2
Shir Amouyal <154405205+ShirAmouyal@users.noreply.github.com>
2024-01-15 16:22:23 +0200

    hw

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,67 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+     
     }
     
     public static int findMissingInt (int [] array) {
         // Write your code here:
-        return -1;
+        int n=array.length;
+        int sum=0;
+        int originalSum=0;
+        originalSum=((n+1)*(n))/2;
+        for(int i=0;i<n;i++){
+            sum+=array[i];
+        }
+        return originalSum-sum;
     }
+   
 
     public static int secondMaxValue(int [] array) {
         // Write your code here:
-        return 0;
+        int max=0;
+        int max2=0;
+        for(int i=0;i<array.length;i++){
+            if(array[i]>max){
+                max=array[i];
+            }
+        }
+       for(int i=0;i<array.length;i++){
+        if(array[i]>max2 && array[i]<max){
+            max2=array[i];
+        }
+       }
+        return max2;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
         // Write your code here:
-        return false;
+       boolean find=false;
+       for(int i=0;i<array1.length;i++){
+        find=false;
+        for(int x=0;x<array2.length;x++){
+          if(array1[i]==array2[x]){
+            find=true;
+          }
+        }
+        if(find==false) return false;
+       }
+       return true;
     }
 
     public static boolean isSorted(int [] array) {
         // Write your code here:
-        return false;
+        boolean sorted=true;
+       if(array[0]>array[1]){// from the max to min
+        for(int i=0;i<array.length-1;i++){
+            if(array[i]<array[i+1]){sorted=false;}
+        }
+       }
+       if(array[0]<array[1]){//from the min to max
+        for(int i=0;i<array.length-1;i++){
+            if(array[i]>array[i+1]){sorted=false;}
+        }
+       }
+        return sorted;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,106 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+     
     }
 
     public static String capVowelsLowRest (String string) {
         // Write your code here:
-        return "";
+        String str="";
+        for(int i=0;i<string.length();i++){
+            if(string.charAt(i)=='a'){
+                str+='A';   
+            }
+            else if(string.charAt(i)=='e'){
+              str+='E';
+            }
+            else if(string.charAt(i)=='i'){
+             str+="I";
+            }
+            else if(string.charAt(i)=='o'){
+             str+="O";
+            }
+            else if(string.charAt(i)=='u' ){
+             str+="U";
+            }
+
+            else if(string.charAt(i)==32){
+                str+=" ";
+            }
+            else if(string.charAt(i)>64 && string.charAt(i)<91){
+                str+=(char) (string.charAt(i)+32) ;
+            }
+            else{
+                str+=(char) string.charAt(i);
+            }
+        }
+        return str;
     }
 
     public static String camelCase (String string) {
         // Write your code here:
-        return "";
+        String str="";
+        //אות גדולה בתו הראשון
+        if(string.charAt(0)>='A'&& string.charAt(0)<='Z'){
+            str+=(char)(string.charAt(0)+32) ;
+        }
+        // אות קטנה בתו הראשון
+        else if(string.charAt(0)>='a'&& string.charAt(0)<='z'){
+            str+=(char)string.charAt(0);
+        }
+        // האם יש רווח לפני המשפט
+        int n=1;
+         while(string.charAt(n)==32){
+         n++;}
+        
+         // האם אחרי הרווח יש אות גדולה
+        if(string.charAt(n)>='A'&& string.charAt(n)<='Z'){
+            str+=(char)(string.charAt(n)+32);
+            n++;
+        }
+        // האם אחרי הרווח האות קטנה
+        else if(string.charAt(n)>='a'&& string.charAt(n)<='z'){
+            str+=(char)string.charAt(n);
+            n++;
+        }
+        // כעת נרוץ על שאר המחרוזת בלי המילה הראשונה
+        for(int i=n;i<string.length();i++){
+            // האם המילה לא ראשונה והיא אות קטנה?
+            if(string.charAt(i-1)==32 && string.charAt(i)>='a'&& string.charAt(i)<='z'){
+                str+=(char)(string.charAt(i)-32);
+            }
+            // האם המילה היא לא ראשונה והיא אות גדולה?
+            else if(string.charAt(i-1)==32&&string.charAt(i)>='A'&& string.charAt(i)<='Z'){
+                str+=(char)(string.charAt(i));
+            }
+            // האם אתה לא האות הראשונה במילה ואתה אות גדולה
+            else if(string.charAt(i-1)!=32 && string.charAt(i)>='A'&& string.charAt(i)<='Z'){
+                str+=(char)(string.charAt(i)+32);
+            }
+            // האם אתה לא האות הראשונה במשפט ואתה אות קטנה
+            else if(string.charAt(i-1)!=32 && string.charAt(i)>='a'&& string.charAt(i)<='z'){
+                str+=(char)(string.charAt(i));
+            }
+        } 
+      return str; 
     }
 
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
-        return new int[1];
+        int length=0;
+        for(int i=0;i<string.length();i++){
+            if(string.charAt(i)==chr){
+            length++;
+            }
+        }
+        int[] arr1=new int[length];
+        int x=0;
+        for(int i=0;i<string.length();i++){
+            if(string.charAt(i)==chr){
+             arr1[x]=i;
+             x++;
+            }  
+        }
+        return arr1;
     }
 }
